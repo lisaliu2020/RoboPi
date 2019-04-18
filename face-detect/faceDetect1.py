3de752c5c82a05daf15b2d4ddc0ad6be838d2f71
lisaisfabu <lisaliu2016@gmail.com>
2019-04-18 15:29:01 -0400

    deleted unusable images, and create description file

--- face-detect/faceDetect1.py
+++ face-detect/faceDetect1.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import os
 
+# store raw images
 def store_raw_imgs():
 	# neg_imgs_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00523513'
 	neg_imgs_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n00007846'
@@ -29,4 +30,40 @@ def store_raw_imgs():
 		except Exception as e:
 			print(str(e))
 
-store_raw_imgs()
+# delete photos that aren't found
+def find_ugly():
+	for file_type in ['neg']:
+		for img in os.listdir(file_type):
+			for ugly in os.listdir('ugly'):
+				try:
+					# get curr img
+					current_img_path = str(file_type)+'/'+str(img)
+					ugly = cv2.imread('ugly/'+str(ugly))
+					question = cv2.imread(current_img_path)
+
+					# if they have the same dimensions  |  xor - one or the other not both
+					# everything is identical
+					if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+						print('u ugly')
+						print(current_img_path)
+						os.remove(current_img_path)
+
+				except Exception as e:
+					print(str(e))
+
+# description file
+def create_pos_n_neg():
+	for file_type in ['neg']:
+		for img in os.listdir(file_type):
+			if file_type == 'neg':
+				line = file_type + '/' +img + '\n'
+				with open('bg.txt','a') as f:
+					f.write(line)
+			elif file_type == 'pos':
+				line = file_type + '/' +img + ' 1 0 0 50 50\n'
+				with open('info.dat', 'a') as f:
+					f.write(line)
+
+# store_raw_imgs()
+# find_ugly()
+create_pos_n_neg()
