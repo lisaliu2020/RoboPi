14f077f5a3ddc323ae1b10726584d2735fd502b2
Lorenzo Galante <lorenzo@galantepi>
2019-05-09 18:29:43 -0400

    Functioning servo diagnostics

---
+++ hardware/ServoControl.py
@@ -0,0 +1,27 @@
+#!/user/bin/env python
+
+import sys
+import time
+import random
+import pigpio
+
+NUM_GPIO=4
+
+MIN_WIDTH=1000
+MAX_WIDTH=2000
+
+pi = pigpio.pi()
+
+if not pi.connected:
+	exit()
+
+
+pi.set_PWM_frequency(NUM_GPIO, 300)
+pi.set_servo_pulsewidth(NUM_GPIO, 1485)
+time.sleep(5)
+pi.set_servo_pulsewidth(NUM_GPIO, 1000)
+time.sleep(5)
+pi.set_servo_pulsewidth(NUM_GPIO, 2000)
+time.sleep(5)
+pi.set_servo_pulsewidth(NUM_GPIO, 0)
+pi.stop()
